1e23627da0f16c62f401e1b10211db5ba64991a5
Voi <1172673619@qq.com>
2017-06-24 21:50:42 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -4,6 +4,6 @@ module.exports = Student(name,age,klass){
  this.klass = klass;
  this.introduce =  function () {
    return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
-}
+ }
  student.prototype =  new Person();
 }
