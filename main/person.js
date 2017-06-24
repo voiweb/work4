7773b93708747a147107249879c19601d258f732
Voi <1172673619@qq.com>
2017-06-24 21:45:18 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,9 +1,11 @@
-// Write your code here
-module.exports = Person(name,age)
+Person(name,age)
 {
 this.name=name;
 this.age=age;
 this.introduce = function(){
 return "My name is Tom. I am 21 years old." ;
 }
-}
+};
+// Write your code here
+module.exports = Person;
+
