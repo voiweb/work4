962ed113126a0089e23e9dc084af56150c6c17dd
voiweb <1172673619@qq.com>
2017-06-24 21:36:11 +0800

    commit

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
-module.exports = Person
+module.exports = Person(name,age)
+{
+this.name=name;
+this.age=age;
+this.introduce = function(){
+return "My name is Tom. I am 21 years old." ;
+}
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,9 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = Student (name,age,class){
+ this.class = class;
+ student.prototype =  new Person();
+ this.introduce =  function () {
+   return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+}
+}
