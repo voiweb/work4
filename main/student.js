21b1486264c397b25ebda3ac26e2177e0bd398b6
Voi <1172673619@qq.com>
2017-06-24 21:58:50 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -2,8 +2,8 @@ let Person = require('./person')
 // Write your code here
 module.exports = Student(name,age,klass){
  this.klass = klass;
- Student.prototype =  new Person;
  this.introduce =  function(){
    return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
  }
+  Student.prototype =  new Person;
 }
